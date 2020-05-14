cfbc6db0b7882669097a3f81fd656c4d94aed864
Wilkins Cheng <wilkins.cheng@gmail.com>
2020-05-14 12:08:14 +1000

    added sorting functionality of conditions

--- app/controllers/booklistings_controller.rb
+++ app/controllers/booklistings_controller.rb
@@ -2,7 +2,12 @@ class BooklistingsController < ApplicationController
   before_action :set_booklisting, only: [:show, :edit, :update, :destroy]
 
   def index
-    @booklistings = Booklisting.all.order("created_at DESC")
+    if params[:condition].blank?
+      @booklistings = Booklisting.all.order("created_at DESC")
+    else
+      @condition_id = Condition.find_by(status: params[:condition]).id
+      @booklistings = Booklisting.where(:condition_id => @condition_id).order("created_at DESC")
+    end 
   end
 
   def show
